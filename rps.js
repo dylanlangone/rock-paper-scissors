14cb89171a379e8d488450894b34bda0ef8f022f
Dylan Langone <dylanlangone628@gmail.com>
2024-12-03 17:22:30 -0500

    Remove old code

--- rps.js
+++ rps.js
@@ -59,10 +59,6 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-/* const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-playRound(humanChoice, computerChoice); */
-
 function playGame() {
     for (let i = 0; i < 5; i++) {
         const humanChoice = getHumanChoice();
