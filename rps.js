38a66331c163a6db6e65aa5a3551346858051368
Dylan Langone <dylanlangone628@gmail.com>
2024-12-02 22:16:52 -0500

    Implement random number generator
    We need a random choice of rock, paper, scissors for the computer

--- rps.js
+++ rps.js
@@ -1,1 +1,9 @@
 console.log("Hello, rps!");
+
+// randomly return rock, paper, or scissors
+function getComputerChoice() {
+    let randomInt = Math.floor(Math.random() * 3);
+    return randomInt;
+}
+
+console.log("random int: " + getComputerChoice());
