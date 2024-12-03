a42e43fb18144458fd442d9e5beafaa755f17771
Dylan Langone <dylanlangone628@gmail.com>
2024-12-02 22:19:52 -0500

    Implement random choice for computer

--- rps.js
+++ rps.js
@@ -3,7 +3,13 @@ console.log("Hello, rps!");
 // randomly return rock, paper, or scissors
 function getComputerChoice() {
     let randomInt = Math.floor(Math.random() * 3);
-    return randomInt;
+    if (randomInt === 0) {
+        return "rock";
+    } else if (randomInt === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
 
-console.log("random int: " + getComputerChoice());
+console.log("random choice: " + getComputerChoice());
